a9be34c16d45ffb3e941b9b8d6dd04e5e76824db
xiaojianggege <2804149747@qq.com>
2020-07-31 17:17:15 +0800

    leetcode 875

---
+++ 875/875.js
@@ -0,0 +1,15 @@
+//尝试一下  暴力 再优化
+/**
+ * 
+ * @param {number[]} piles 
+ * @param {number} H 
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, H) {
+  let lo = 1
+     hi = Math.max(...piles)
+  // 求出最大香蕉数,在某堆中
+  return lo
+}
+
+minEatingSpeed([3, 6, 7, 11], 8)
